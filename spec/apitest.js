0abc47a0763b6d3eea3424739fdc67636aaed095
Aidan Eglin <aidaneglin@Aidans-MacBook-Pro.local>
2019-06-12 19:41:03 -0700

    Project Structure Created

--- spec/apitest.js
+++ spec/apitest.js
@@ -6,8 +6,8 @@ describe("Hello world server", () => {
     describe("GET /api/v1/users", () => {
         it("returns an object where the okay param is yes", () => {
             request.get(base_url + "api/v1/users/", (err, res, body) => {
-                expect(body).toBe({"yeee": "gottem"});
-                // done();
+                expect(body).toBe({});
+                done();
             });
         });
     });
