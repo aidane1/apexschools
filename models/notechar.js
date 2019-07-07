89bc21f7a05db29ac4bfa207e442376a6fcba319
Aidan Eglin <aidaneglin@Aidans-MacBook-Pro.local>
2019-07-06 20:51:15 -0700

    app api

--- models/notechar.js
+++ models/notechar.js
@@ -1,8 +1,26 @@
 const mongoose = require("mongoose");
 
-const UserSchema = mongoose.Schema({
-
+const NoteSchema = mongoose.Schema({
+    uploaded_by: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "account",
+    },
+    note: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: new Date(),
+    },
+    reference_course: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "course",
+    },
+    resources: {
+        type: [{type: mongoose.Schema.Types.ObjectId, ref: "topic"}],
+    }
 });
 
-const User = mongoose.model("note", UserSchema);
-module.exports = User;
+const Note = mongoose.model("note", NoteSchema);
+module.exports = Note;
