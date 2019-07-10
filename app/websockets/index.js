d6e91b24880f5c7611e1d80d19a435693dec771c
Aidan Eglin <aidaneglin@Aidans-MacBook-Pro.local>
2019-07-10 12:51:36 -0700

    added

--- app/websockets/index.js
+++ app/websockets/index.js
@@ -2,33 +2,43 @@ const express = require("express");
 
 const router = express.Router();
 
-module.exports = function(expressWs) {
-    router.ws("/app/courses/:course", (ws, req) => {
-        console.log(req.path);
-        console.log(ws);
-        ws.on("message", async (msg) => {
-            try {
-                msg = JSON.parse(msg);
-                console.log(msg);
-                msg.date = new Date();
-                let course = await models.course.findById(req.params.course);
-                let { account, key } = await models.apikey.authenticate(msg["x-id-key"], msg["x-api-key"]);
-                msg.reference_course = course._id;
-                msg.school = course.school;
-                msg.uploaded_by = account._id;
-                msg.username = account.username;
-                let textMessage = await models["course-text"].create(msg);
-                aWss.clients.forEach(client => {
-                    client.send(JSON.stringify(textMessage));
-                });
-            } catch(e) {
-                console.log(e);
-                ws.send(JSON.stringify({
-                    status: "error",
-                    body: "An error occured",
-                }));
-            }
-        })
-    });
-    return router;
-}
+let clients = {
+
+};
+
+router.ws("/app/courses/:course", (ws, req) => {
+    ws.getUniqueID = function () {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+        }
+        return s4() + s4() + '-' + s4();
+    };
+    ws.id = ws.getUniqueID();
+    clients[ws.id] = ws;
+    console.log(clients);
+    ws.on("message", async (msg) => {
+        try {
+            msg = JSON.parse(msg);
+            console.log(msg);
+            msg.date = new Date();
+            let course = await models.course.findById(req.params.course);
+            let { account, key } = await models.apikey.authenticate(msg["x-id-key"], msg["x-api-key"]);
+            msg.reference_course = course._id;
+            msg.school = course.school;
+            msg.uploaded_by = account._id;
+            msg.username = account.username;
+            let textMessage = await models["course-text"].create(msg);
+            aWss.clients.forEach(client => {
+                client.send(JSON.stringify(textMessage));
+            });
+        } catch(e) {
+            console.log(e);
+            ws.send(JSON.stringify({
+                status: "error",
+                body: "An error occured",
+            }));
+        }
+    })
+});
+
+module.exports = router;
