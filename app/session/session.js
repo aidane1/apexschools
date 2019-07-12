488be924925d90f0a150087307bd1ef25d3498df
Aidan Eglin <aidaneglin@Aidans-MacBook-Pro.local>
2019-07-11 19:37:19 -0700

    added

--- app/session/session.js
+++ app/session/session.js
@@ -1,12 +1,13 @@
 
 const session = require("express-session");
 const MongoStore = require('connect-mongo')(session);
+const mongoose = require("mongoose");
 
 let sess = {
     cookie: {},
     resave: false,
     saveUninitialized: true,
-    store: new MongoStore(options)
+    store: new MongoStore({mongooseConnection: mongoose.connection}),
 }
 
 module.exports = function() {
