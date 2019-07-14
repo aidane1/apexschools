41d7ca950b577b00c651bf37dc4edea2bd158c43
Aidan Eglin <aidaneglin@Aidans-MacBook-Pro.local>
2019-07-14 10:55:24 -0700

    added

--- push_notifications/index.js
+++ push_notifications/index.js
@@ -3,7 +3,7 @@ const { Expo } = require('expo-server-sdk');
 let expo = new Expo();
 
 sendPushNotifications = async () => {
-    let users = await global.models.users.find({push_token: {$exists: true, $not: {$size: 0}}});
+    let users = await global.models.user.find({push_token: {$exists: true, $not: {$size: 0}}});
     console.log(users);
 }
 
