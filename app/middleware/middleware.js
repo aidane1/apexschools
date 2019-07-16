0f4fc1b1afdb2940864b1e807309682e2787e47c
Aidan Eglin <aidaneglin@Aidans-MacBook-Pro.local>
2019-07-16 11:24:05 -0700

    added

--- app/middleware/middleware.js
+++ app/middleware/middleware.js
@@ -1,71 +1,88 @@
-const middlewareDev = require(__dirname + "/middleware_dev");
+const middlewareDev = require (__dirname + '/middleware_dev');
 
-const middlewareProd = require(__dirname + "/middleware_prod");
+const middlewareProd = require (__dirname + '/middleware_prod');
 
-const middlewareAPI = require(__dirname + "/api_request_auth");
+const middlewareAPI = require (__dirname + '/api_request_auth');
 
-const fileUpload = require('express-fileupload');
+const fileUpload = require ('express-fileupload');
 
-const helmet = require("helmet");
+const helmet = require ('helmet');
 
-const bodyParser = require("body-parser");
+const bodyParser = require ('body-parser');
 
-const addRequestId = require('express-request-id');
+const addRequestId = require ('express-request-id');
 
-const express = require("express");
+const express = require ('express');
 
-const fs = require("fs");
+const fs = require ('fs');
 
-let morganLogStream = fs.createWriteStream(abs_path("/morgan.log"), {flags: "a"});
+let morganLogStream = fs.createWriteStream (abs_path ('/morgan.log'), {
+  flags: 'a',
+});
 
-const morgan = require("morgan");
+const morgan = require ('morgan');
 
-morgan.token("id", (req) => {
-    return req.id;
+morgan.token ('id', req => {
+  return req.id;
 });
 
 let loggerFormat = ':id [:date[web]] ":method :url" :status :response-time ms';
 
 let middleware = {
-    helmet: helmet(),
-    fileUpload: fileUpload(),
-    bodyParserForm: bodyParser.urlencoded({extended: false, limit: "50mb"}),
-    staticServing: express.static(base_dir + "/public/"),
-    bodyParserJSON: bodyParser.json({limit: "50mb"}),
-    responseError: (req, res, next) => {
-        res.error = (error) => {
-            res.send({
-                status: "error",
-                body: {
-                    error
-                }
-            })
-        }
-        next();
-    },
-    responseOkay: (req, res, next) => {
-        res.okay = (body) => {
-            res.send({
-                status: "ok",
-                body,
-            })
-        }
-        next();
-    },
-    morganOne: morgan(loggerFormat,{
-        skip: (req, res) => {
-            return res.statusCode > 400;
+  helmet: helmet (),
+  fileUpload: fileUpload (),
+  bodyParserForm: bodyParser.urlencoded ({extended: false, limit: '50mb'}),
+  staticServing: express.static (base_dir + '/public/'),
+  bodyParserJSON: bodyParser.json ({limit: '50mb'}),
+  rawBody: (req, res, next) => {
+    getRawBody (
+      req,
+      {
+        length: req.headers['content-length'],
+        limit: '50mb',
+        encoding: contentType.parse (req).parameters.charset,
+      },
+      function (err, string) {
+        if (err) return next (err);
+        req.text = string;
+        next ();
+      }
+    );
+  },
+  responseError: (req, res, next) => {
+    res.error = error => {
+      res.send ({
+        status: 'error',
+        body: {
+          error,
         },
-        stream: morganLogStream,
-    }),
-    addRequestId: addRequestId(),
-}
-module.exports = function() { 
-    if (server_info.config.config_id == "development") {
-        middleware = {...middleware, ...middlewareDev};
-    } else {
-        middleware = {...middleware, ...middlewareProd};
-    }
-    middleware = {...middleware, ...middlewareAPI};
-    return middleware;
+      });
+    };
+    next ();
+  },
+  responseOkay: (req, res, next) => {
+    res.okay = body => {
+      res.send ({
+        status: 'ok',
+        body,
+      });
+    };
+    next ();
+  },
+  morganOne: morgan (loggerFormat, {
+    skip: (req, res) => {
+      return res.statusCode > 400;
+    },
+    stream: morganLogStream,
+  }),
+  addRequestId: addRequestId (),
+};
+module.exports = function () {
+  if (server_info.config.config_id == 'development') {
+    middleware = {...middleware, ...middlewareDev};
+  } else {
+    middleware = {...middleware, ...middlewareProd};
+  }
+  middleware = {...middleware, ...middlewareAPI};
+  return middleware;
 };
