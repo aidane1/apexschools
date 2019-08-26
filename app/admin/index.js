5e818e82b6fbff6fb9bd127a556e3e6e96401cad
Aidan Eglin <aidaneglin@Aidans-MacBook-Pro.local>
2019-08-26 10:00:58 -0700

    added

--- app/admin/index.js
+++ app/admin/index.js
@@ -1,8 +1,32 @@
-const express = require("express");
-const router = express.Router();
+const express = require ('express');
+const router = express.Router ();
 
-router.get("/*", async (req, res) => {
-    res.sendFile(abs_path("/react-client/dist/index.html"));
+router.get ('/*', async (req, res) => {
+  try {
+    let headers = req.headers;
+    let cookies = req.cookies;
+    // console.log (cookies);
+    let apikey = headers['x-api-key'] || cookies['x-api-key'];
+    let accountID = headers['x-id-key'] || cookies['x-id-key'];
+
+    if (apikey) {
+      if (accountID) {
+        let {account, key} = await models.apikey.authenticate (
+          accountID,
+          apikey
+        );
+        if (account.permission_level >= 3) {
+          res.sendFile (abs_path ('/react-client/dist/index.html'));
+        }
+      } else {
+        res.redirect ('/login');
+      }
+    } else {
+      res.redirect ('/login');
+    }
+  } catch (e) {
+    res.redirect ('/login');
+  }
 });
 
 module.exports = router;
