6c900977cc0651fd0eaca542cacf0715f985c963
Aidan Eglin <aidaneglin@Aidans-MacBook-Pro.local>
2019-09-07 12:04:30 -0700

    added

--- scheduled_events/index.js
+++ scheduled_events/index.js
@@ -1,19 +1,29 @@
-const moment = require ('moment-timezone');
+const moment = require("moment-timezone");
 
 let scheduleEvent = (time, callback) => {
-  setInterval (() => {
-    let date = moment (new Date ()).tz ('America/Vancouver');
-    if (date.hours () === time.hour && date.minutes () === time.minute) {
-      callback ();
+  setInterval(() => {
+    let date = moment(new Date()).tz("America/Vancouver");
+    if (date.hours() === time.hour && date.minutes() === time.minute) {
+      callback();
     }
   }, 60000);
 };
 
+let intervalEvent = (interval, callback) => {
+  setInterval(callback, interval);
+};
+
 module.exports = () => {
-  scheduleEvent ({hour: 7, minute: 30}, async () => {
-    await global.models['user'].updateMany (
+  scheduleEvent({ hour: 7, minute: 30 }, async () => {
+    await global.models["user"].updateMany(
       {},
-      {$set: {unviewed_chatrooms: []}}
+      { $set: { unviewed_chatrooms: [] } }
+    );
+  });
+  intervalEvent(10 * 60 * 1000, async () => {
+    await global.models["user"].updateMany(
+      {},
+      { $set: { unviewed_chatrooms: [] } }
     );
   });
 };
